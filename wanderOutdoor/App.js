93f76eb5fd3f5aa3359b4a6659593cece49e3341
Huck <evanallen51@gmail.com>
2018-02-22 13:39:03 -0600

    Testing Slack/GitHub integration

--- wanderOutdoor/App.js
+++ wanderOutdoor/App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Wander Outdoor!</Text>
+        <Text>We are on Slack and Trello now!</Text>
       </View>
     );
   }
